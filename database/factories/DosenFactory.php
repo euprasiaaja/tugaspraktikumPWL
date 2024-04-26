f988a499455c254cae9156dbab775fb55de31c15
euprasiaenjelika <151954982+euprasiaaja@users.noreply.github.com>
2024-04-27 00:15:45 +0700

    Add files via upload

---
+++ database/factories/DosenFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Dosen;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Dosen>
+ */
+class DosenFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Dosen::class;
+
+    public function definition(): array
+    {
+        return [
+            'nama' => $this->faker->name(),
+            'nip' => $this->faker->numerify('##########'),
+            'jabatan' => $this->faker->jabatan(),
+            'matakuliah' => $this->faker->matakuliah(),
+            'email' => $this->faker->unique->safeEmail(),
+];
+}
+}
